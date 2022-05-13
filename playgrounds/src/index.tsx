6593f2434bb6f93156cd489cf149af0ffb1d53c5
netogerbi <netogerbi2@hotmail.com>
2022-05-13 17:01:55 -0300

    creating the first atomic component for colored squares

--- playgrounds/src/index.tsx
+++ playgrounds/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { Button } from "@ds.e/react";
+import { Color } from "@ds.e/react";
 
-import "@ds.e/scss/lib/Button";
+import "@ds.e/scss/lib/Utilities.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<Button label="Example Button" />);
+root.render(<Color hexCode="#000" width="lg" height="xxxl" />);
